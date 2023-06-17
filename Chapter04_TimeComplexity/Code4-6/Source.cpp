536a12d8b2e6c7887d7cec6ffb0b546683329f95
SooHyeon Jeong <jsh75721@naver.com>
2023-06-17 18:15:29 +0900

    복습

--- Chapter04_TimeComplexity/Code4-6/Source.cpp
+++ Chapter04_TimeComplexity/Code4-6/Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // 자연수 n의 소인수 분해 결과를 담은 정수 배열을 반환
 vector<int> factor(int n) {
 	if (n == 1)
-		return vector<int>(1, 1);	// n=1인 경우는 예외로 한다.
+		return vector<int>(1, 1);   // n=1인 경우는 예외로 한다.
 	vector<int> ret;
 	for (int div = 2; n > 1; ++div) {
 		while (n % div == 0) {
