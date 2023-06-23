570084bc45fed77bfc4e8476b891d0e2841533e9
SooHyeon Jeong <jsh75721@naver.com>
2023-06-24 00:41:12 +0900

    6.5 소풍 마무리 6.6 게임판 덮기 진행 중

--- Chapter06_BruteForce/Code6-5/Source.cpp
+++ Chapter06_BruteForce/Code6-5/Source.cpp
@@ -1,6 +1,5 @@
 /*
-6.5 소풍 문제를 해결하는 (잘못된) 재귀 호출 코드
-
+6.5 소풍 문제를 해결하는 재귀 호출 코드
 */
 
 #include <iostream>
@@ -11,22 +10,25 @@ int n;	// 친구 수
 bool areFriends[10][10];
 // taken[i] = i번째 학생이 짝을 이미 찾았으면 true, 아니면 false
 int countPairings(bool taken[10]) {
+	// 남은 학생 중 가장 번호가 빠른 학생을 찾는다.
+	int firstFree = -1;
+	for (int i = 0; i < n; ++i) {
+		if (!taken[i]) {
+			firstFree = i;
+			break;
+		}
+	}
 	// 기저 사례 : 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다.
-	bool finished = true;
-	for (int i = 0; i < n; ++i)
-		if (!taken[i])
-			finished = false;
-	if (finished)
-		return 1;
+	if (firstFree == -1)	return 1;
 	int ret = 0;
-	// 서로 친구인 두 학생을 찾아 짝을 지어 준다.
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			if (!taken[i] && !taken[j] && areFriends[i][j]) {
-				taken[i] = taken[j] = true;
-				ret += countPairings(taken);
-				taken[i] = taken[j] = false;
-			}
+	// 이 학생과 짝지을 학생을 결정한다.
+	for (int pairWith = firstFree + 1; pairWith < n; ++pairWith) {
+		if (!taken[pairWith] && areFriends[firstFree][pairWith]) {
+			taken[firstFree] = taken[pairWith] = true;
+			ret += countPairings(taken);
+			taken[firstFree] = taken[pairWith] = false;
+		}
+	}
 	return ret;
 }
 
@@ -39,12 +41,12 @@ int main() {
 	bool isFriend = false;
 	int f1 = -1, f2 = -1;
 	for (int k = 0; k < c; ++k) {
+		memset(taken, false, sizeof(taken));
+		memset(areFriends, false, sizeof(areFriends));
 		cin >> n >> m;	// 친구 수, 친구 쌍의 수
 		for (int i = 0; i < m; ++i) {
-			for (int j = 0; j < m; ++j)	// 초기화
-				areFriends[i][j] = false;
 			cin >> f1 >> f2;
-			areFriends[f1][f2] = true;
+			areFriends[f1][f2] = areFriends[f2][f1] = true;
 		}
 		cout << countPairings(taken) << endl;
 	}
