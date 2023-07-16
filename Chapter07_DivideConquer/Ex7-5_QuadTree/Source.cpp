69a086428746a33cc98056eb9f591af38c9e6bab
SooHyeon Jeong <jsh75721@naver.com>
2023-07-16 20:01:28 +0900

    7.2 문제 : 쿼드 트리 뒤집기 다시 해봐야 돼

---
+++ Chapter07_DivideConquer/Ex7-5_QuadTree/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_SIZE 2000
+
+char decompressed[MAX_SIZE][MAX_SIZE];
+// s를 압축 해제해서 decompressed[y..y + size - 1][x..x + size -1 ] 구간에 쓴다
+void decompress(const string& s, int y, int x, int size) {
+
+}
+
+int main() {
+	int c;	// 테스트 케이스 (c<=50)
+	// 문자열 길이 1000이하, 원본 그림의 크기 2^20 * 2^20
+	string quadStr;
+
+	cin >> quadStr;
+
+
+	return 0;
+}
