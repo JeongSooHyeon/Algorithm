fc807087010f7aeaaa7b76e6851604d205dd5f35
SooHyeon Jeong <jsh75721@naver.com>
2023-09-10 22:45:20 +0900

    8장 동적 계획법 Quantization (ing)

---
+++ Chpater08_DynamicProgramming/Code8-15_Quantization-DP/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	int c;	// 테스트 케이스 1 <= c <= 50
+	int n;	// 수열의 길이 1 <= n <= 100
+	int s;	// 사용할 숫자의 수 1 <= s <= 10
+	for (int i = 0; i < c; i++) {
+		cin >> n >> s;
+
+	}
+
+	return 0;
+}
