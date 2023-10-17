22e944ee130fa11618274a37d79794509272cb70
SooHyeon Jeong <jsh75721@naver.com>
2023-10-17 17:06:43 +0900

    21장 트리 끝

--- Chapter21-Tree/Code21-5_Fortress/Main.cpp
+++ Chapter21-Tree/Code21-5_Fortress/Main.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 using namespace std;
 
+int n;	// 성벽의 수 1<=n<=100
+
 struct TreeNode {
 	vector<TreeNode*> children;
 };
@@ -41,22 +43,55 @@ int solve(TreeNode* root) {
 	return max(longest, h);
 }
 
+// 입력 데이터
+int y[100], x[100], radius[100];	// x,y,z : 위치와 크기, 0<=x, y<=1000, 0<=i<n
+// x^2을 반환한다
+int sqr(int x) {
+	return x * x;
+}
+// 두 성벽 a,b의 중심점 간의 거리의 제곱을 반환
+int sqrdist(int a, int b) {
+	return sqr(y[a] - y[b]) + sqr(x[a] - x[b]);
+}
+// 성벽 a가 성벽 b를 포함하는지 확인한다
+bool encloses(int a, int b) {
+	return radius[a] > radius[b] && sqrdist(a, b) < sqr(radius[a] - radius[b]);
+}
+// '성벽'트리에서 parent가 child의 부모인지 확인한다
+// parent는 child를 꼭 직접 포함해야 한다.
+bool isChild(int parent, int child) {
+	if (!encloses(parent, child))	// parent가 child를 포함하는지
+		return false;
+	for (int i = 0; i < n; i++) {
+		if (i != parent && i != child && encloses(parent, i) && encloses(i, child))	// i는 parent, child가 아님, parent에 포함되고 child를 포함한다(parent와 child 사이에 다른 성벽이 있다.)
+			return false;
+	}
+	return true;
+}
+
+// 주어진 번호의 성벽에 포함된 구역들을 표현하는 트리를 생성
+// root 성벽을 루트로 하는 트리 생성
+TreeNode* getTree(int root) {
+	TreeNode* ret = new TreeNode();
+	for (int ch = 0; ch < n; ch++) {
+		// ch 성벽이 root 성벽에 직접적으로 포함되어 있다면
+		// 트리를 만들고 자손 목록에 추가
+		if (isChild(root, ch))
+			ret->children.push_back(getTree(ch));
+	}
+	return ret;
+}
+
 int main() {
 	int c;	// 케이스 수
 	cin >> c;
-	int n;	// 성벽의 수, 1<=n<=100
 
-	int walls[100][3];	// (x,y)를 중심으로 하는 반지금 r인 원형
-	int x, y, z;	// x,y,z : 위치와 크기, 0<=x, y<=1000, 0<=i<n
-	
-	
 	for (int i = 0; i < c; i++) {
 		cin >> n;
 		for (int j = 0; j < n; j++) {
-			cin >> walls[j][0] >> walls[j][1] >> walls[j][2];	// 첫번째 입력이 외벽
-
-			// 함수 호출
-
+			cin >> x[j] >> y[j] >> radius[j];	// 첫번째 입력이 외벽
 		}
+		TreeNode* tree = getTree(0);	// 0을 루트로 하는 트리 생성
+		cout << solve(tree) << endl;
 	}
 }
