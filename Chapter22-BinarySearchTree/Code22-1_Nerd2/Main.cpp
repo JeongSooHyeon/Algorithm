23117692cee8a28a002c9cec95edef2176c77cb7
SooHyeon Jeong <jsh75721@naver.com>
2023-10-20 21:43:23 +0900

    이진탐색트리 너드 하는 중

---
+++ Chapter22-BinarySearchTree/Code22-1_Nerd2/Main.cpp
@@ -0,0 +1,28 @@
+/*
+한 점이 다른 점에 지배당하는지 확인하는 함수
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+	int c;	
+	cin >> c;
+
+	int n;	// 참가 신청한 사람들의 수	10<=n<=50000
+	int p, q;	// 0<=p,q<=100000
+	vector<vector<int>> v;
+	for(int i = 0; i < c; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+
+		}
+	}
+
+
+
+
+	return 0;
+}
